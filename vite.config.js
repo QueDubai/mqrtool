f4931ec90753881566d8ffe9e1bfc692702ea0a8
SuperFuhq <499690234@qq.com>
2026-07-02 10:52:45 +0800

    chore: rename

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// 项目站点：https://quedubai.github.io/mqrtool.github.io/
+// 项目站点：https://quedubai.github.io/mqrtool/
 export default defineConfig({
   plugins: [vue()],
-  base: '/mqrtool.github.io/',
+  base: '/mqrtool/',
 })
